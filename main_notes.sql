ddb5767c2c5777c3c4fed7b767f38657c24e1cce
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-07-04 15:05:46 -0600

    Starting our repo

---
+++ main_notes.sql
@@ -0,0 +1,41 @@
+/*
+
+*HOW TO CREATE A NEW DB In SQL:
+
+We write:
+
+CREATE DATABASE database_name
+
+*   Important concepts of a database:
+
+*Record: This is a tuple inside a table of a ER database.
+
+*Field: This is the column inside a table of a ER database.
+
+*Field value:   This is the value inside the space of a "field" and a "record"
+
+
+*FIELD TYPES:
+
+*INTEGER:   This is for save numbers in the field.
+
+*TEXT:  This is for save chars and strings in the field.
+
+*BLOB: This is for save binaries like files, videos or images and in general multimedia content in the field.
+
+*REAL:  This is for save floats and numeric information like percentages and that stuff in the field (Important to see that real works with a 8 bytes info and is faster that "NUMERIC" to work).
+
+*NUMERIC:   This is for save in the field by the more specific and accurate way very big numbers, a good example is PI (Numeric field can save data without a limit of bytes and is slower than "REAL" for work).
+
+
+*A VERY IMPORTANT DETAIL: SQLlite works with a dinamic field system, baically every value field will auto become to the type of data that we put there (this is a bad practice).
+
+
+*FEATURES OF FIELDS:
+
+    We can write "DEFAULT" for define the value that our field will always have 
+
+
+
+--We stop at 47:41
+*/
